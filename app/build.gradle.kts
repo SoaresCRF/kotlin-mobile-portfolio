22420079cb700b41f0b2ea1608c75a724dfbf21f
SoaresCRF <103540321+SoaresCRF@users.noreply.github.com>
2025-10-14 17:39:34 -0300

    feat: add download feature for project ZIP files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.dev.soarescrf.soares"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
